821fd5801daf16ad020c74c6cebba4359eb9a7f7
isabelburgos <isabelmburgos01@gmail.com>
2024-03-18 15:45:45 -0700

    define raster frame counter logic in simulation

--- Gateware/glasgowcontrib/applet/open_beam_interface/test.py
+++ Gateware/glasgowcontrib/applet/open_beam_interface/test.py
@@ -1,8 +1,9 @@
 import unittest
-from amaranth.sim import Simulator, Tick
+from amaranth import *
+from amaranth.sim import Simulator, Tick, Active
 
 from . import StreamSignature
-from . import Supersampler, RasterScanner
+from . import Supersampler, RasterScanner, RasterRegion
 
 
 def put_stream(stream, data):
@@ -41,7 +42,7 @@ def get_stream(stream, data):
 
 
 class OBIAppletTestCase(unittest.TestCase):
-    def simulate(self, dut, testbenches):
+    def simulate(self, dut, testbenches, setup = None):
         sim = Simulator(dut)
         sim.add_clock(20.83e-9)
         for testbench in testbenches:
@@ -105,25 +106,63 @@ class OBIAppletTestCase(unittest.TestCase):
     def test_raster_scanner(self):
         dut = RasterScanner()
 
-        def put_testbench():
+        x_start = 5
+        x_count = 3
+        x_step = 0x2_00
+        y_start = 9
+        y_count = 2
+        y_step = 0x5_00
+
+        dwell_times = [1,2,3,7,8,9]
+
+        def setup_testbench():
             yield from put_stream(dut.roi_stream, {
-                "x_start": 5, "x_count": 3, "x_step": 0x2_00,
-                "y_start": 9, "y_count": 2, "y_step": 0x5_00,
+                "x_start": x_start, "x_count": x_count, "x_step": x_step,
+                "y_start": y_start, "y_count": y_count, "y_step": y_step,
             })
-            yield from put_stream(dut.dwell_stream, 1)
-            yield from put_stream(dut.dwell_stream, 2)
-            yield from put_stream(dut.dwell_stream, 3)
-            yield from put_stream(dut.dwell_stream, 7)
-            yield from put_stream(dut.dwell_stream, 8)
-            yield from put_stream(dut.dwell_stream, 9)
+
+        def put_testbench():
+            for dwell_time in dwell_times:
+                yield from put_stream(dut.dwell_stream, dwell_time)
+
+        def frame(x_start, x_count, x_step, y_start, y_count, y_step):
+            _x_step = float(x_step/256)
+            _y_step = float(y_step/256)
+            x_accum = x_start
+            y_accum = y_start
+            for y in range(y_count+1):
+                for x in range(x_count+1):
+                    yield x_accum, y_accum
+                    x_accum += _x_step
+                x_accum = x_start
+                y_accum += _y_step
+
+        self.frame = frame(x_start, x_count, x_step, y_start, y_count, y_step)
 
         def get_testbench():
-            yield from get_stream(dut.dac_stream, {"dac_x_code": 5, "dac_y_code": 9,  "dwell_time": 1})
-            yield from get_stream(dut.dac_stream, {"dac_x_code": 7, "dac_y_code": 9,  "dwell_time": 2})
-            yield from get_stream(dut.dac_stream, {"dac_x_code": 9, "dac_y_code": 9,  "dwell_time": 3})
-            yield from get_stream(dut.dac_stream, {"dac_x_code": 5, "dac_y_code": 11, "dwell_time": 7})
-            yield from get_stream(dut.dac_stream, {"dac_x_code": 7, "dac_y_code": 11, "dwell_time": 8})
-            yield from get_stream(dut.dac_stream, {"dac_x_code": 9, "dac_y_code": 11, "dwell_time": 9})
+            for dwell_time in dwell_times:
+                x_accum, y_accum = next(self.frame)
+                yield from get_stream(dut.dac_stream, 
+                        {"dac_x_code":x_accum, "dac_y_code":y_accum, "dwell_time":dwell_time})
             assert (yield dut.dac_stream.valid) == 0
 
-        self.simulate(dut, [put_testbench, get_testbench])
+        self.simulate(dut, [setup_testbench, put_testbench, get_testbench])
+
+
+
+                            
+
+
+            # yield from get_stream(dut.dac_stream, {"dac_x_code": 5, "dac_y_code": 9,  "dwell_time": 1})
+            # yield from get_stream(dut.dac_stream, {"dac_x_code": 7, "dac_y_code": 9,  "dwell_time": 2})
+            # yield from get_stream(dut.dac_stream, {"dac_x_code": 9, "dac_y_code": 9,  "dwell_time": 3})
+            # yield from get_stream(dut.dac_stream, {"dac_x_code": 5, "dac_y_code": 11, "dwell_time": 7})
+            # yield from get_stream(dut.dac_stream, {"dac_x_code": 7, "dac_y_code": 11, "dwell_time": 8})
+            # yield from get_stream(dut.dac_stream, {"dac_x_code": 9, "dac_y_code": 11, "dwell_time": 9})
+
+            # yield from put_stream(dut.dwell_stream, 1)
+            # yield from put_stream(dut.dwell_stream, 2)
+            # yield from put_stream(dut.dwell_stream, 3)
+            # yield from put_stream(dut.dwell_stream, 7)
+            # yield from put_stream(dut.dwell_stream, 8)
+            # yield from put_stream(dut.dwell_stream, 9)
