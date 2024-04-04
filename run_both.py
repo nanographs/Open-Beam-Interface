0b5c1068060f64fb16177df68d637efb4070ad0d
isabelburgos <isabelmburgos01@gmail.com>
2024-04-04 14:06:24 -0700

    wip: scripts to install obi software and glasgow applet, and configure pinout based on microscope configuration file

---
+++ run_both.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+import tomllib
+
+config = tomllib.load(open("microscope.toml", "rb") )
+pinout = config["pinout"]
+pin_args = []
+for pin_name in pinout:
+    pin_num = pinout.get(pin_name)
+    pin_args += ["--pin-"+pin_name, str(pin_num)]
+glasgow_cmd = ["glasgow", "run", "open_beam_interface", "-V", "5"] + pin_args + ["tcp::2223"]
+print(glasgow_cmd)
+
+env = os.environ._data
+env.update({"GLASGOW_OUT_OF_TREE_APPLETS":"I-am-okay-with-breaking-changes"})
+subprocess.Popen(glasgow_cmd,env=env)
+subprocess.Popen(["obi_gui"])
+
