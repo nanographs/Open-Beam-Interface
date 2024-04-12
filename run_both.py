158d05506af686617cc95c539c35868b2e7fdd5a
isabelburgos <isabelmburgos01@gmail.com>
2024-04-12 13:59:02 -0700

    add port arg to run_both

--- run_both.py
+++ run_both.py
@@ -2,6 +2,11 @@ import subprocess
 import os
 import tomllib
 import pathlib
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("port")
+args = parser.parse_args()
 
 
 config_path = pathlib.Path("Open-Beam-Interface/microscope.toml").expanduser()
@@ -12,10 +17,11 @@ pin_args = []
 for pin_name in pinout:
     pin_num = pinout.get(pin_name)
     pin_args += ["--pin-"+pin_name, str(pin_num)]
-glasgow_cmd = ["glasgow", "run", "open_beam_interface", "-V", "5"] + pin_args + ["tcp::2223"]
+endpoint_arg = ["tcp::" + args.port]
+glasgow_cmd = ["glasgow", "run", "open_beam_interface", "-V", "5"] + pin_args + endpoint_arg
 
 env = os.environ._data
 env.update({"GLASGOW_OUT_OF_TREE_APPLETS":"I-am-okay-with-breaking-changes"})
 subprocess.Popen(glasgow_cmd,env=env)
-subprocess.Popen(["obi_gui", "--config_path", config_path])
+subprocess.Popen(["obi_gui", "--config_path", config_path, args.port])
 
