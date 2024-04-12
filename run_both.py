90f1fcf6a0b2c97834c6141d078f76990dfd3eed
isabelburgos <isabelmburgos01@gmail.com>
2024-04-12 13:33:37 -0700

    get absolute path when loading config file

--- run_both.py
+++ run_both.py
@@ -1,18 +1,19 @@
 import subprocess
 import os
 import tomllib
+import pathlib
 
-config = tomllib.load(open("microscope.toml", "rb") )
+config_path = pathlib.Path("microscope.toml").expanduser()
+config = tomllib.load(open(config_path, "rb") )
 pinout = config["pinout"]
 pin_args = []
 for pin_name in pinout:
     pin_num = pinout.get(pin_name)
     pin_args += ["--pin-"+pin_name, str(pin_num)]
 glasgow_cmd = ["glasgow", "run", "open_beam_interface", "-V", "5"] + pin_args + ["tcp::2223"]
-print(glasgow_cmd)
 
 env = os.environ._data
 env.update({"GLASGOW_OUT_OF_TREE_APPLETS":"I-am-okay-with-breaking-changes"})
 subprocess.Popen(glasgow_cmd,env=env)
-subprocess.Popen(["obi_gui", "--config-path", "microscope.toml"])
+subprocess.Popen(["obi_gui", "--config-path", config_path])
 
