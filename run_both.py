83613b52ef64ed5b7c0a4cf47de463c67e67932e
isabelburgos <isabelmburgos01@gmail.com>
2024-04-12 18:46:21 -0700

    kill glasgow process when gui process is terminated

--- run_both.py
+++ run_both.py
@@ -22,12 +22,15 @@ glasgow_cmd = ["glasgow", "run", "open_beam_interface", "-V", "5"] + pin_args + 
 
 env = os.environ._data
 env.update({"GLASGOW_OUT_OF_TREE_APPLETS":"I-am-okay-with-breaking-changes"})
-# subprocess.Popen(glasgow_cmd,env=env)
+
 obi_cmd = ["obi_gui", "--config_path", config_path, args.port]
 if "gui_settings" in config:
     if "window_size" in config["gui_settings"]:
         obi_cmd += ["--window_size"]
         obi_cmd += [str(x) for x in config["gui_settings"]["window_size"]]
-print(f"{obi_cmd=}")
-subprocess.Popen(obi_cmd)
+
+glasgow = subprocess.Popen(glasgow_cmd,env=env)
+obi = subprocess.Popen(obi_cmd)
+obi.wait()
+glasgow.kill()
 
