3bff955646fcbc2e7c30b00fec439068c8bd2334
isabelburgos <isabelmburgos01@gmail.com>
2024-05-10 15:27:38 -0700

    delete redundant file

--- Gateware/scripts/blank_test_2.py
+++
@@ -1,8 +0,0 @@
-from glasgowcontrib.applet.open_beam_interface.base_commands import *
-
-seq = CommandSequence()
-## seq.add(Command().message)
-## ...
-
-await iface.write(seq)
-response = await iface.read()
