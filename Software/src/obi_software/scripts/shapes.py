55df2bc4546a6c391cdf45fc2a2ec90ad67e9bff
isabelburgos <isabelmburgos01@gmail.com>
2024-05-10 17:49:13 -0700

    improve shapes cli

--- Software/src/obi_software/scripts/shapes.py
+++ Software/src/obi_software/scripts/shapes.py
@@ -4,14 +4,17 @@ import asyncio
 import argparse
 import matplotlib.pyplot as plt
 import logging
-from ..beam_interface import Connection, VectorPixelLinearRunCommand, _ExternalCtrlCommand, _BeamSelectCommand, BeamType, setup_logging
+import sys
+from ..stream_interface import Connection, VectorPixelLinearRunCommand, _ExternalCtrlCommand, _BeamSelectCommand, BeamType, setup_logging
+from ..base_commands import CommandSequence, VectorPixelCommand, SynchronizeCommand, ExternalCtrlCommand, BlankCommand, OutputMode, BeamType, SelectBeamCommand
 
 setup_logging({"Stream": logging.DEBUG})
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--show', action='store_true', help="show the pattern")
+parser.add_argument('--show', action='store_true', help="show the pattern with matplotlib")
+parser.add_argument('--buffer', action='store_true', help="write to stdout")
 parser.add_argument('--dwell', type=int, help="dwell time per pixel", default=2)
-args = parser.parse_args()
+
 
 def circle(r):
     for degree in range(360):
@@ -46,8 +49,28 @@ def scale_to_dac_range(f):
     y = y.astype("int")
     return x, y
 
+parser.add_argument('--n_points', type=int, help="number of points", default=1000)
+parser.add_argument('--repeats', type=int, help="number times to repeat the pattern", default=1)
+subparsers = parser.add_subparsers(title='shapes',
+                                description='valid shapes',
+                                help='options are rose and spiral')
+parser_rose = subparsers.add_parser('rose')
+parser_rose.add_argument('--a', type=int, help="see rose()", default=2)
+parser_rose.add_argument('--b', type=int, help="see rose()", default=5)
+parser_rose.set_defaults(rose=True, spiral=False)
+parser_spiral = subparsers.add_parser('spiral')
+parser_spiral.add_argument('--n_points', type=int, help="see spiral()", default=1000)
+parser_spiral.add_argument('--n_loops', type=int, help="see spiral()", default=10)
+parser_spiral.set_defaults(spiral=True, rose=False)
+args = parser.parse_args()
+print(f"{args=}")
+if args.rose:
+    pattern_func = rose(args.a, args.b, args.n_points)
+if args.spiral:
+    pattern_func = spiral(args.n_points, args.n_loops)
 
-x, y  = scale_to_dac_range(polar_to_cartesian(rose(2, 5, 1000)))
+# x, y  = scale_to_dac_range(polar_to_cartesian(rose(args.a, args.b, 1000)))
+x, y  = scale_to_dac_range(polar_to_cartesian(pattern_func))
 def show(x, y):
     plt.plot(x, y)
     plt.show()
@@ -61,16 +84,33 @@ async def stream_pattern(x, y, dwell):
     conn = Connection('localhost', 2224)
     await conn.transfer(_ExternalCtrlCommand(enable=True))
     await conn.transfer(_BeamSelectCommand(beam_type=BeamType.Ion))
-    pattern = iterpattern(x, y, dwell, repeats=15)
+    pattern = iterpattern(x, y, dwell, repeats=args.repeats)
     async for chunk in conn.transfer_multiple(VectorPixelLinearRunCommand(pattern_generator=pattern), 
                                         output_mode=0):
         pass
     #await conn.transfer(_ExternalCtrlCommand(enable=False, beam_type=BeamType.Ion))
 
-if args.show:
-    show(x, y)
-else:
-    while True:
-        asyncio.run(stream_pattern(x, y, args.dwell))
+def buffer_pattern(x, y, dwell):
+    pattern = iterpattern(x, y, dwell, repeats=args.repeats)
+    seq = CommandSequence(output=OutputMode.NoOutput, raster = False)
+    seq.add(BlankCommand(enable=True))
+    seq.add(SelectBeamCommand(beam_type = BeamType.Ion))
+    seq.add(ExternalCtrlCommand(enable=True))
+    seq.add(BlankCommand(enable=False, inline=True))
+    for x, y, dwell in pattern:
+        seq.add(VectorPixelCommand(x_coord = x, y_coord = y, dwell = dwell))
+    seq.add(BlankCommand(enable=True))
+    seq.add(ExternalCtrlCommand(enable=False))
+    sys.stdout.buffer.write(seq.message)
+
+
+def main():
+    if args.show:
+        show(x, y)
+    if args.buffer:
+        buffer_pattern(x, y, args.dwell)
+    else:
+        while True:
+            asyncio.run(stream_pattern(x, y, args.dwell))
     
 
