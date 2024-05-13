d3da2271e8895f0a6288476510cd79b21d0b2c6d
isabelburgos <isabelmburgos01@gmail.com>
2024-05-13 15:08:57 -0700

    update command syntax

--- Gateware/scripts/blank_test.py
+++ Gateware/scripts/blank_test.py
@@ -5,17 +5,17 @@ loops = 100
 
 seq = CommandSequence(output=OutputMode.NoOutput, raster=False)
 seq.add(BlankCommand())
-seq.add(EnableExtCtrlCommand())
-seq.add(SelectIbeamCommand())
+seq.add(ExternalCtrlCommand(enable=False))
+seq.add(BeamSelectCommand(beam_type=BeamType.Ion))
 
 def dash_line(y):
-    seq.add(UnblankInlineCommand())
+    seq.add(BlankCommand(enable=False, inline=True))
     for x in range(4096):
         seq.add(VectorPixelCommand(x_coord = x, y_coord=y, dwell=dwell))
     seq.add(BlankCommand())
     for x in range(4096, 12288):
         seq.add(VectorPixelCommand(x_coord = x, y_coord=y, dwell=dwell))
-    seq.add(UnblankInlineCommand())
+    seq.add(BlankCommand(enable=False, inline=True))
     for x in range(12288,16383):
         seq.add(VectorPixelCommand(x_coord = x, y_coord=y, dwell=dwell))
     seq.add(BlankCommand())
@@ -25,7 +25,7 @@ for _ in range(loops):
     dash_line(12288)
 
 
-seq.add(DisableExtCtrlCommand())
+seq.add(ExternalCtrlCommand(enable=False))
 
 for _ in range(loops):
     await iface.write(seq.message)
