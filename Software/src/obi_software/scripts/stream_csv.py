339cd79a38a6a5a4fc5b62c66331b33dae07bdd1
isabelburgos <isabelmburgos01@gmail.com>
2024-05-16 14:01:08 -0700

    fix dependencies + command syntax (needs more work)

--- Software/src/obi_software/scripts/stream_csv.py
+++ Software/src/obi_software/scripts/stream_csv.py
@@ -1,17 +1,21 @@
 import numpy as np
 import asyncio
 import argparse
+import pathlib
 import matplotlib.pyplot as plt
 import logging
-from ..beam_interface import Connection, VectorPixelLinearRunCommand, _ExternalCtrlCommand, BeamType, setup_logging, DACCodeRange
+from ..stream_interface import Connection, VectorPixelLinearRunCommand, _ExternalCtrlCommand, BeamType, setup_logging, DACCodeRange
 from ..frame_buffer import FrameBuffer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dwell', type=int, help="dwell time per pixel", default=2)
+parser.add_argument('--csv_path', required=True, 
+                    type=lambda p: pathlib.Path(p).expanduser(), #expand paths starting with ~ to absolute
+                    help='path to csv file with two columns, x and y')
 args = parser.parse_args()
 
 def import_csv(dwell):
-    file = open("amaranth-logo.csv")
+    file = open(args.csv_path)
 
     x_coords = []
     y_coords = []
@@ -32,7 +36,7 @@ async def stream_pattern():
     conn = Connection('localhost', 2224)
     fb = FrameBuffer(conn)
     photo_range = DACCodeRange(0, 4096, int((16384/4096)*256))
-    await conn.transfer(_ExternalCtrlCommand(enable=True, beam_type=BeamType.Ion))
+    await conn.transfer(_ExternalCtrlCommand(enable=True))
     async for chunk in conn.transfer_multiple(VectorPixelLinearRunCommand(pattern_generator=pattern), 
                                         output_mode=0):
         pass
