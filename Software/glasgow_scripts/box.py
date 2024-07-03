7fd773de3877c2610fa7ee8aabd01cfb9548ab84
isabelburgos <isabelmburgos01@gmail.com>
2024-07-03 13:22:23 -0700

    update obi_script command and box script

--- Software/glasgow_scripts/box.py
+++ Software/glasgow_scripts/box.py
@@ -1,16 +1,16 @@
-from glasgowcontrib.applet.open_beam_interface.base_commands import *
+from Gateware.applet.open_beam_interface.base_commands import *
 
 seq = CommandSequence(output=OutputMode.NoOutput, raster=False)
 ## seq.add(Command())
 ## ...
-seq.add(BlankCommand())
+seq.add(BlankCommand(enable=True, inline=False))
 seq.add(ExternalCtrlCommand(enable=True))
-seq.add(DelayCommand(5760))
+seq.add(DelayCommand(delay=5760))
 seq.add(BeamSelectCommand(beam_type=BeamType.Ion))
 seq.add(BlankCommand(enable=False, inline=True))
 
 
-dwell = 65536
+dwell = 1
 
 
 def box(x_start, y_start, x_width, y_height):
@@ -18,16 +18,16 @@ def box(x_start, y_start, x_width, y_height):
     ##       |    |
     ##       ------ x = x_start + x_width, y = y_start + y_height
     for x in range(x_start, x_start+x_width):
-        seq.add(VectorPixelCommand(x_coord=x, y_coord = y_start, dwell = dwell))
+        seq.add(VectorPixelCommand(x_coord=x, y_coord = y_start, dwell_time = dwell))
         print(f"{x=}")
     for y in range(y_start, y_start+y_height):
-        seq.add(VectorPixelCommand(x_coord=x_start+x_width, y_coord = y, dwell = dwell))
+        seq.add(VectorPixelCommand(x_coord=x_start+x_width, y_coord = y, dwell_time = dwell))
         print(f"{y=}")
     for x in range(x_start+x_width, x_start, -1):
-        seq.add(VectorPixelCommand(x_coord=x, y_coord = y_start+y_height, dwell = dwell))
+        seq.add(VectorPixelCommand(x_coord=x, y_coord = y_start+y_height, dwell_time = dwell))
         print(f"{x=}")
     for y in range(y_start+y_height, y_start, -1):
-        seq.add(VectorPixelCommand(x_coord=x_start, y_coord = y, dwell = dwell))
+        seq.add(VectorPixelCommand(x_coord=x_start, y_coord = y, dwell_time = dwell))
         print(f"{y=}")
 
 
@@ -35,6 +35,6 @@ box(5000, 5000, 5000, 5000)
 
 seq.add(ExternalCtrlCommand(enable=False))
 
-
-await iface.write(seq.message)
+print("writing")
+await iface.write(bytes(seq))
 await iface.flush()
