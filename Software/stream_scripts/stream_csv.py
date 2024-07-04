494aeac47707aaf1e4d2ce77c9420b5ccd496e96
isabelburgos <isabelmburgos01@gmail.com>
2024-07-04 14:41:15 -0700

    vectorpixellinearrun -> vectorpixeliter

--- Software/stream_scripts/stream_csv.py
+++ Software/stream_scripts/stream_csv.py
@@ -4,8 +4,9 @@ import argparse
 import pathlib
 import matplotlib.pyplot as plt
 import logging
-from ..stream_interface import Connection, VectorPixelLinearRunCommand, _ExternalCtrlCommand, BeamType, setup_logging, DACCodeRange
-from ..frame_buffer import FrameBuffer
+from ..stream_interface import TCPConnection
+from base_commands import *
+#from ..frame_buffer import FrameBuffer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dwell', type=int, help="dwell time per pixel", default=2)
@@ -36,15 +37,15 @@ async def stream_pattern():
     conn = Connection('localhost', 2224)
     fb = FrameBuffer(conn)
     photo_range = DACCodeRange(0, 4096, int((16384/4096)*256))
-    await conn.transfer(_ExternalCtrlCommand(enable=True))
-    async for chunk in conn.transfer_multiple(VectorPixelLinearRunCommand(pattern_generator=pattern), 
+    await conn.transfer(ExternalCtrlCommand(enable=True))
+    async for chunk in conn.transfer_multiple(VectorPixelIter(pattern_generator=pattern), 
                                         output_mode=0):
         pass
-    full_frame = None
-    async for frame in fb.capture_frame(x_range = photo_range, y_range = photo_range, dwell=4, latency=65536):
-        full_frame = frame
-    await conn.transfer(_ExternalCtrlCommand(enable=False, beam_type=BeamType.Ion))
-    full_frame.saveImage_tifffile(save_dir="")
+    # full_frame = None
+    # async for frame in fb.capture_frame(x_range = photo_range, y_range = photo_range, dwell=4, latency=65536):
+    #     full_frame = frame
+    await conn.transfer(ExternalCtrlCommand(enable=False))
+    # full_frame.saveImage_tifffile(save_dir="")
 
 asyncio.run(stream_pattern())
 
