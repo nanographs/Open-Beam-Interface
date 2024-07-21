ff7a109c1ee35dfaf6ce5650be8796121bc0ab3d
isabelburgos <isabelmburgos01@gmail.com>
2024-07-21 16:21:21 -0700

    add more test modules

---
+++ software/tests/test_commands/test_structs.py
@@ -0,0 +1,10 @@
+import unittest
+from commands.structs import DACCodeRange
+
+
+class DACCodeRangeTest(unittest.TestCase):
+    def test_from_resolution(self):
+        def test_out_of_range():
+            self.assertRaises(ValueError, lambda: DACCodeRange.from_resolution(16385))
+            self.assertRaises(ValueError, lambda: DACCodeRange(0, 16385, 256))
+        test_out_of_range()
