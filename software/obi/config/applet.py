9b8109150be63f307ea711a088be1d677bb42db6
isabelburgos <isabelmburgos01@gmail.com>
2024-08-23 09:13:56 -0700

    config.applet: add optional rich printed table

--- software/obi/config/applet.py
+++ software/obi/config/applet.py
@@ -1,7 +1,14 @@
 import argparse
 
 from glasgow.access.direct.arguments import PinArgument
-from rich import print
+
+try:
+    from rich import print
+    from rich.table import Table
+    from rich.console import Console
+    has_rich = True
+except:
+    has_rich = False
 
 
 class OBIAppletArguments:
@@ -12,13 +19,19 @@ class OBIAppletArguments:
                 pin_set_ebeam_scan_enable=None, pin_set_ibeam_scan_enable=None,
                 pin_set_ebeam_blank_enable=None, pin_set_ibeam_blank_enable=None,
                 pin_set_ebeam_blank=None, pin_set_ibeam_blank=None,
-                xflip=None, yflip=None, rotate90=None,
+                xflip=None, yflip=None, rotate90=None, pin_set_line_clock=None, pin_set_frame_clock=None,
                 loopback=None, out_only=None, benchmark=None, ext_switch_delay=None,
                 endpoint=('tcp', 'localhost', 2224))
     def load_toml(self):
-        import tomllib
-        self.toml = tomllib.load(open(self.path, "rb") )
+        from tomlkit.toml_file import TOMLFile
+        self.toml_file = TOMLFile(self.path)
+        self.toml = self.toml_file.read()
     def parse_toml(self):
+        if has_rich:
+            table = Table(title="pinout")
+            table.add_column("name")
+            table.add_column("number")
+            table.add_column("invert")
         if self.toml is None:
             self.load_toml()
         config = self.toml
@@ -35,6 +48,12 @@ class OBIAppletArguments:
                         pins = [PinArgument(num) if num > 0 else PinArgument(abs(num), invert=True) for num in pin_num ]
                         print(pins)
                         setattr(self.args, pin_name, pins)
+                        if has_rich:
+                            for pin in pins:
+                                table.add_row(str(pin_name), str(pin.number), str(pin.invert))
+            if has_rich:
+                console = Console()
+                console.print(table)
         if "transforms" in config:
             transforms = config["transforms"]
             for transform, setting in transforms.items():
