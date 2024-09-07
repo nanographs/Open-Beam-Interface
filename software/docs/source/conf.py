016c43996b228acdfdc5f5a0bddac8b21836862e
isabelburgos <isabelmburgos01@gmail.com>
2024-09-07 16:05:50 -0400

    docs: add extension to link code sections to source

--- software/docs/source/conf.py
+++ software/docs/source/conf.py
@@ -18,6 +18,7 @@ extensions = [
     "myst_parser", 
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.linkcode",
     "enum_tools.autoenum"
     ]
 
@@ -32,9 +33,32 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 html_static_path = ['_static']
+html_favicon = '_static/favicon.png'
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#FF4F00",
+        "color-brand-content": "#2EA22A",
+        "color-brand-visited": "#1F7B1E"
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#FF4F00",
+        "color-brand-content": "#2EA22A",
+        "color-brand-visited": "#1F7B1E"
+    },
+    "light_logo": "image_scan_drawing_black.svg",
+    "dark_logo": "image_scan_drawing_white.svg"
+}
+
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    filename = info['module'].replace('.', '/')
+    return "https://github.com/nanographs/Open-Beam-Interface/blob/update-main/software/%s.py" % filename
+    
